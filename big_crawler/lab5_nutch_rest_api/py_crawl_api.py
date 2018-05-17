6146263c4bc5580d29884d5e52d014c82683055b
Jan Ischebeck <j.ischebeck@reply.de>
2018-05-17 09:59:32 +0200

    demo for python requests

---
+++ big_crawler/lab5_nutch_rest_api/py_crawl_api.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 17 09:12:48 2018
+
+@author: j.ischebeck
+"""
+
+import requests
+
+NUTCH_URL = 'http://159.122.175.139:30081'
+
+class ApiError:
+    def __init__(self, error):
+        self.error=error
+
+test_id = "12345"
+#%%
+
+# Seed
+seed = {"id": test_id,"name": "doandodge","seedUrls": [{"id": 1,"seedList": None,
+       "url": "http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/"} ]}
+
+resp = requests.post(NUTCH_URL+'/seed/create', json=seed)
+if resp.status_code != 201:
+    raise ApiError('POST /seed/create/ {}'.format(resp.status_code))
+
+print('Created Seed. ID: {}'.format(resp.json()["id"]))
+
+#%%
+seedDir = "/tmp/1526463295128-0"  # comes from first request
+crawl_id = "sample-crawl-01"
+
+# inject
+job_def = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "INJECT"}
+
+resp = requests.post(NUTCH_URL+'/job/create', json=job_def)
+if resp.status_code != 201:
+    raise ApiError('POST /job/create/ {}'.format(resp.status_code))
+
+print('Created Job data: {}'.format(resp.json()))
+
+#%%
