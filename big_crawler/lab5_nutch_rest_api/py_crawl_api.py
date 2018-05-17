5e11199d49815686ebbdac789d7606e4c781394a
Hiekel Friedrich <f.hiekel@reply.de>
2018-05-17 15:31:29 +0200

    py crawler v2

--- big_crawler/lab5_nutch_rest_api/py_crawl_api.py
+++ big_crawler/lab5_nutch_rest_api/py_crawl_api.py
@@ -6,21 +6,22 @@ Created on Thu May 17 09:12:48 2018
 """
 
 import requests
-
+import time
+from random import randint
 NUTCH_URL = 'http://159.122.175.139:30081'
 
 class ApiError:
     def __init__(self, error):
         self.error=error
 
-test_id = "123456"
+test_id = "1234567"
 #%%
 
 # Seed
 
 
 seed = {"id": test_id,"name": "doandodge1","seedUrls": [{"id": 1,"seedList": None,
-       "url": "http://nutch.apache.org/"} ]}  #http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/
+       "url": "http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/"} ]}  #http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/
 
 resp = requests.post(NUTCH_URL+'/seed/create', json=seed)
 if resp.status_code != (201 and 200):
@@ -32,8 +33,9 @@ print('Created Seed. ID: {}'.format(resp.text))
 seedDir = resp.text #"/tmp/1526547667623-0"  # comes from first request  1526463295128-0
 crawl_id = "sample-crawl-01"
 
+
 # inject
-job_def = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "INJECT"}
+job_def = {"args": {"seedDir": seedDir}, "confId":"default", "crawlId":crawl_id, "type": "INJECT"}
 
 resp = requests.post(NUTCH_URL+'/job/create', json=job_def)
 if resp.status_code != (201 and 200):
@@ -41,9 +43,22 @@ if resp.status_code != (201 and 200):
 
 print('Created Job data: {}'.format(resp.text))
 
-generate = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "GENERATE"}
-fetch = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "FETCH"}
-parse = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "PARSE"}
-updatedb = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "UPDATEDB"}
 
+n = 5
+
+
+for x in range(0, 5):
+    time1 = round(time.time())
+    batch = str(time1) + "-" + str(randint(1000, 9999))
+    generate = {"args": {     "normalize": False, "filter": True,"crawlId": crawl_id, "curTime": time1, "batch": batch}, "confId":"default","crawlId":crawl_id, "type": "GENERATE"}
+    resp_generate = requests.post(NUTCH_URL + '/job/create', json=generate)
+
+    fetch = {"args": {"threads": 50, "crawlId" : crawl_id, "batch" : batch},"confId":"default","crawlId":crawl_id ,"type": "FETCH"}
+    resp_fetch = requests.post(NUTCH_URL + '/job/create', json=fetch)
+
+    parse = {"args": {"crawlId" : crawl_id, "batch" : batch}, "confId":"default","crawlId":crawl_id ,"type": "PARSE"}
+    resp_parse = requests.post(NUTCH_URL + '/job/create', json=parse)
+
+    updatedb = {"args": {"crawlId" : crawl_id, "batch" : batch },"confId":"default", "crawlId":crawl_id ,"type": "UPDATEDB"}
+    resp_updatedb = requests.post(NUTCH_URL+'/job/create', json=updatedb)
 #%%
