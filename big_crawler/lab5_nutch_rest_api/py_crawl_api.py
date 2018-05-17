f28d05e2af2eeb50d0184ee716be6d70eadbc00b
Hiekel Friedrich <f.hiekel@reply.de>
2018-05-17 14:04:38 +0200

    py crawler

--- big_crawler/lab5_nutch_rest_api/py_crawl_api.py
+++ big_crawler/lab5_nutch_rest_api/py_crawl_api.py
@@ -13,30 +13,37 @@ class ApiError:
     def __init__(self, error):
         self.error=error
 
-test_id = "12345"
+test_id = "123456"
 #%%
 
 # Seed
-seed = {"id": test_id,"name": "doandodge","seedUrls": [{"id": 1,"seedList": None,
-       "url": "http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/"} ]}
+
+
+seed = {"id": test_id,"name": "doandodge1","seedUrls": [{"id": 1,"seedList": None,
+       "url": "http://nutch.apache.org/"} ]}  #http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/
 
 resp = requests.post(NUTCH_URL+'/seed/create', json=seed)
-if resp.status_code != 201:
+if resp.status_code != (201 and 200):
     raise ApiError('POST /seed/create/ {}'.format(resp.status_code))
 
-print('Created Seed. ID: {}'.format(resp.json()["id"]))
+print('Created Seed. ID: {}'.format(resp.text))
 
 #%%
-seedDir = "/tmp/1526463295128-0"  # comes from first request
+seedDir = resp.text #"/tmp/1526547667623-0"  # comes from first request  1526463295128-0
 crawl_id = "sample-crawl-01"
 
 # inject
 job_def = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "INJECT"}
 
 resp = requests.post(NUTCH_URL+'/job/create', json=job_def)
-if resp.status_code != 201:
+if resp.status_code != (201 and 200):
     raise ApiError('POST /job/create/ {}'.format(resp.status_code))
 
-print('Created Job data: {}'.format(resp.json()))
+print('Created Job data: {}'.format(resp.text))
+
+generate = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "GENERATE"}
+fetch = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "FETCH"}
+parse = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "PARSE"}
+updatedb = {"args": {"seedDir": seedDir },"crawlId":crawl_id ,"type": "UPDATEDB"}
 
 #%%
