1f238a280f8217122a11b9fcf66f3f141deddd4e
Hiekel Friedrich <f.hiekel@reply.de>
2018-05-22 11:29:01 +0200

    nutch job status fix

--- big_crawler/lab5_nutch_rest_api/py_crawl_api.py
+++ big_crawler/lab5_nutch_rest_api/py_crawl_api.py
@@ -8,6 +8,7 @@ Created on Thu May 17 09:12:48 2018
 import requests
 import time
 from random import randint
+import json
 NUTCH_URL = 'http://159.122.175.139:30081'
 
 class ApiError:
@@ -15,13 +16,18 @@ class ApiError:
         self.error=error
 
 test_id = "12348"
-#%%
 
 # Seed
 
+def getJobStatus(jobid, seedDir, config):
+    resp_status = requests.get(NUTCH_URL+'/job/'+jobid)
+    status_json = json.loads(resp_status.text)
+    status_msg = status_json['state']
+    return status_msg
+
 
 seed = {"id": test_id,"name": "mynutch","seedUrls": [{"id": 1,"seedList": None,
-       "url": "http://www.lra-aoe.de"} ]}  #http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/ http://nutch.apache.org/
+       "url": "http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/"} ]}  #http://www.region-suedostoberbayern.bayern.de/verbandsarbeit/sitzungen/ http://nutch.apache.org/
 # http://nutch.apache.org/     https://www.lra-aoe.de/   https://www.wikipedia.org/
 resp = requests.post(NUTCH_URL+'/seed/create', json=seed)
 if resp.status_code != (201 and 200):
@@ -44,57 +50,65 @@ if resp.status_code != (201 and 200):
 print('Created Job data: {}'.format(resp.text))
 
 
-n = 5
-configdb = {
-"configId":"test1",
-"force": "false",
-"params" : {
-    "nutch.conf.uuid":"test1",
-    "mapred.reduce.tasks.speculative.execution":False,
-    "mapred.map.tasks.speculative.execution" : False,
-    "mapred.compress.map.output" : True,
-    "mapred.reduce.tasks" : 2,
-    "fetcher.timelimit.mins": 180,
-    "mapred.skip.attempts.to.start.skipping" : 2,
-    "mapred.skip.map.max.skip.records" : 1,
-}
-}
-
-#resp_config= requests.post(NUTCH_URL+'/config/test1', json=configdb)
 config = "default" #resp_config.text #"default"
-print(config)
+#print(config)
 
-for x in range(0, 5):
+for x in range(0, 2):
     print(x)
     time1 = int(round(time.time()*1000))
     batch = str(time1) + "-" + str(randint(1000, 9999))
     print(time1)
-    print(batch)
+    #print(batch)
     generate = {"args": {"normalize": False, "filter": True,"crawlId": crawl_id, "curTime": time1, "batch": batch}, "confId":config,"crawlId":crawl_id, "type": "GENERATE"}
     resp_generate = requests.post(NUTCH_URL + '/job/create', json=generate)
-# "topN": 800,
+    while True:
+        msg = getJobStatus(resp_generate.text, seedDir, config)
+        if msg == ("FINISHED"):
+            print("generate")
+            break;
+        else:
+            time.sleep(1)
     if resp_generate.status_code != (201 and 200):
         raise ApiError('POST /job/create/ {}' + format(resp_generate.status_code))
-    time.sleep(2)
+
 
     fetch = {"args": {"threads": 50, "crawlId" : crawl_id, "batch" : batch},"confId":config,"crawlId":crawl_id ,"type": "FETCH"}
     resp_fetch = requests.post(NUTCH_URL + '/job/create', json=fetch)
+    while True:
+        msg = getJobStatus(resp_fetch.text, seedDir, config)
+        if msg == ("FINISHED"):
+            print("fetch")
+            break;
+        else:
+            time.sleep(1)
     if resp_fetch.status_code != (201 and 200):
         raise ApiError('POST /job/create/ {}' + format(resp_fetch.status_code))
-    time.sleep(2)
+
 
     parse = {"args": {"crawlId" : crawl_id, "batch" : batch}, "confId":config,"crawlId":crawl_id ,"type": "PARSE"}
     resp_parse = requests.post(NUTCH_URL + '/job/create', json=parse)
+    while True:
+        msg = getJobStatus(resp_parse.text, seedDir, config)
+        if msg == ("FINISHED"):
+            print("parse")
+            break;
+        else:
+            time.sleep(1)
     if resp_parse.status_code != (201 and 200):
         raise ApiError('POST /job/create/ {}' + format(resp_parse.status_code))
-    time.sleep(2)
+
 
     updatedb = {"args": {"crawlId" : crawl_id, "batch" : batch },"confId":config, "crawlId":crawl_id ,"type": "UPDATEDB"}
     resp_updatedb = requests.post(NUTCH_URL+'/job/create', json=updatedb)
+    while True:
+        msg = getJobStatus(resp_updatedb.text, seedDir, config)
+        if msg == ("FINISHED"):
+            print("updatedb")
+            break;
+        else:
+            time.sleep(1)
     if resp_updatedb.status_code != (201 and 200):
         raise ApiError('POST /job/create/ {}' + format(resp_updatedb.status_code))
-    time.sleep(2)
-    #index = {"startKey":"com.google", "endKey":"com.yahoo", "isKeysReversed":"true"}
-    #resp_index = requests.get(NUTCH_URL + '/admin')
-    print("test")
-#%%
+
+    print("--------------------------")
+
