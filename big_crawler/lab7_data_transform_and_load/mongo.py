4b15f97c059c09c5c3c9c17d4d70dd49f12ab431
Jan Ischebeck <j.ischebeck@reply.de>
2018-05-25 21:07:59 +0200

    add text classifier plus training data

---
+++ big_crawler/lab7_data_transform_and_load/mongo.py
@@ -0,0 +1,125 @@
+from pymongo import MongoClient
+import json
+import os
+import base64
+import traceback
+#%%
+from elastic import Elastic
+#%%
+import pickle
+filename = 'text_model.sav'
+# load the model from disk
+classify2 = pickle.load(open(filename, 'rb'))
+#%%
+z = classify2.predict(['''eiterte Fassung) vor. Diese Online-Fassung enthält  neben dem Anhang als Teil 
+                des  Bayer-Konzernabschlusses  weiterführende Informatio-nen  zum Lagebericht.
+                Den „Geschäftsbericht 2017 – Erweiterte  Fassung“ finden Sie unter www.bayer.de/GB17
+ A Zusammengefasster Lagebericht
+ Bayer-Geschäftsbericht 2017 1EBITDAvor Sondereinflüssen KonzernergebnisUmsatzLieferantenmanagement
+ Investitionen in Forschung und EntwicklungBereinigtesErgeb'''])
+print(z)
+# %%
+  
+import pdfminer.high_level
+import io
+
+def get_text_from_pdf_2(text):
+    outp = io.StringIO()
+    with io.StringIO(text) as fp:
+        pdfminer.high_level.extract_text_to_fp(fp,outp, output_type='text')
+    text = outp.getvalue()
+    outp.close()
+    return text
+# %%
+
+client = MongoClient('mongodb://159.122.175.139:30017')
+db = client['crawler']
+
+collection = db.webpage
+#%%
+elastic = Elastic()
+
+#%%
+cursor = collection.find({})
+for document in cursor:
+    try:
+        metadata = ""
+
+        if document.get('metadata') != None:
+            try:
+                metadata = {
+                (key if isinstance(key, str) else key.decode('utf-8')): (val if isinstance(val, str) else val.decode('CP1252')) for
+                key, val in document['metadata'].items()}
+            except Exception as e:
+                try:
+                    metadata = {
+                        (key if isinstance(key, str) else key.decode('utf-8')): (
+                        val if isinstance(val, str) else val.decode('utf-16')) for
+                        key, val in document['metadata'].items()}
+                except Exception as e:
+                    print(traceback.format_exc())
+
+        doc = {
+            'inlinks': document['inlinks'] if document.get('inlinks')!= None else "",
+            'baseUrl': document['baseUrl'] if document.get('baseUrl')!= None else "",
+            'contentType': document['contentType'] if document.get('contentType')!= None else "",
+            'title': document['title'] if document.get('title')!= None else "",
+            'text': document['text'] if document.get('text')!= None else "",
+            'metdadata': metadata,
+
+        }
+        
+        if doc['contentType']=='text/pdf':
+            text = get_text_from_pdf_2(document['content'])
+        else:
+            text =  doc['text']
+
+        z = classify2.predict([text])
+        print("%s - %s " % (doc['title'],z[0]))
+        
+        if z[0]!="ignore":
+            
+            doc_id = document['_id']
+            elastic.insert_dokument(doc=doc, doc_id=doc_id)
+
+    except Exception as e:
+        print(document['_id'])
+        print(traceback.format_exc())
+
+#%%
+import sys
+sys.exit(0)
+
+#%%
+
+file = collection.find_one({'_id': 'de.bayern.region-suedostoberbayern.www:http/verbandsarbeit/sitzungen/'})
+binary = file['content']
+name = file['_id'].replace('/', '#').replace(':', ';')
+
+        
+
+#%%
+with open(os.path.expanduser('~/Desktop/data/nutch/'+name+'.html' ), 'wb') as file_out:
+    # fout.write(base64.decodebytes(binary))
+    file_out.write(binary)
+    #print(doc_id.replace('/', '#'))
+'''
+'''
+
+metadata =  { (key if isinstance(key,str) else key.decode('utf-8')) :(val if isinstance(val,str) else val.decode('cp1252')) for key, val in file['metadata'].items() }
+
+doc = {
+    'inlinks': file['inlinks'],
+    'baseUrl': file['baseUrl'],
+    'contentType': file['contentType'],
+    'title': file['title'],
+    'text': file['text'],
+    'metdadata': metadata,
+
+}
+doc_id = file['_id']
+
+elastic = Elastic()
+elastic.insert_dokument(doc=doc, doc_id=doc_id)
+
+'''
---
+++ big_crawler/lab7_data_transform_and_load/prepare_training_data.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 25 19:28:37 2018
+
+@author: j.ischebeck
+"""
+
+#%%
+import os
+os.chdir('D:\\DEV\\bigdata_training\\big_crawler\\lab7_data_transform_and_load')
+#%%
+import PyPDF2 
+
+def get_text_from_pdf(filename):
+    pdfReader = PyPDF2.PdfFileReader(filename)
+    num_pages = pdfReader.numPages
+    count = 0
+    text = ""
+    while count < num_pages:
+        pageObj = pdfReader.getPage(count)
+        count +=1
+        text += pageObj.extractText()
+    return text
+   
+# %%
+  
+import pdfminer.high_level
+import io
+
+def get_text_from_pdf_2(filename):
+    outp = io.StringIO()
+    with open(filename, "rb") as fp:
+        pdfminer.high_level.extract_text_to_fp(fp,outp, output_type='text')
+    text = outp.getvalue()
+    outp.close()
+    return text
+# %%
+
+# prepare PDF to text conversion
+import os
+
+training_data = []
+training_categories = []
+
+path= os.path.join(os.getcwd(),'training_data')
+for dirname in os.listdir(path):
+    subdir = os.path.join(path,dirname)
+    print("scanning files in %s" % dirname)
+    for fname in os.listdir(subdir):      
+        #print("convert file %s"% fname)
+        text = get_text_from_pdf_2(os.path.join(subdir,fname))
+        training_categories.append(dirname)
+        training_data.append (text)
+#%%
+print("\n".join(training_data[1].split("\x0c")[:2]))
+#%%
+print("\n".join(training_data[21].split("\x0c")[:5]))
+#%%        
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+# build pipeline - counting
+classify = Pipeline([('vect', CountVectorizer(stop_words='english')), 
+                     ('tfidf', TfidfTransformer()), 
+                     ('clf', MultinomialNB())])
+
+classify = classify.fit(training_data, training_categories)
+#%%
+z = classify.predict(['''eiterte Fassung) vor. Diese Online-Fassung enthält  neben dem Anhang als Teil 
+                des  Bayer-Konzernabschlusses  weiterführende Informatio-nen  zum Lagebericht.
+                Den „Geschäftsbericht 2017 – Erweiterte  Fassung“ finden Sie unter www.bayer.de/GB17
+ A Zusammengefasster LageberichtFehler! Kein Text mit angegebener Formatvorlage im Dokument.
+ Bayer-Geschäftsbericht 2017 1EBITDAvor Sondereinflüssen KonzernergebnisUmsatzLieferantenmanagement
+ Investitionen in Forschung und EntwicklungBereinigtesErgeb'''])
+print(z)
+#%%
+z = classify.predict([training_data[21]])
+print(z)
+   
+#%% 
+import numpy as np
+z = classify.predict(training_data)
+print(z)    
+print(training_categories)
+np.mean(z == training_categories)
+#%%
+from sklearn.linear_model import SGDClassifier
+classify2 = Pipeline([('vect', CountVectorizer()), 
+                      ('tfidf', TfidfTransformer()),
+                      ('clf-svm', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, max_iter=5, random_state=42))])
+
+classify2 = classify2.fit(training_data, training_categories)
+#%%
+z = classify2.predict([training_data[21]])
+print(z)
+   
+#%% 
+import numpy as np
+z = classify2.predict(training_data)
+print(z)    
+print(training_categories)
+np.mean(z == training_categories)
+#%%
+z = classify2.predict(['''eiterte Fassung) vor. Diese Online-Fassung enthält  neben dem Anhang als Teil 
+                des  Bayer-Konzernabschlusses  weiterführende Informatio-nen  zum Lagebericht.
+                Den „Geschäftsbericht 2017 – Erweiterte  Fassung“ finden Sie unter www.bayer.de/GB17
+ A Zusammengefasster Lagebericht
+ Bayer-Geschäftsbericht 2017 1EBITDAvor Sondereinflüssen KonzernergebnisUmsatzLieferantenmanagement
+ Investitionen in Forschung und EntwicklungBereinigtesErgeb'''])
+print(z)
+#%%
+import pickle
+filename = 'text_model.sav'
+pickle.dump(classify2, open(filename, 'wb'))
+#%%
