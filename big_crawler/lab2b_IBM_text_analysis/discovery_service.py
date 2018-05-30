d1a8c588397cf11b34f08402ee5bab97c98d81b7
Andreas Kaiser <an.kaiser@reply.de>
2018-05-30 14:24:30 +0000

    Replace discovery_service.py

--- big_crawler/lab2b_IBM_text_analysis/discovery_service.py
+++ big_crawler/lab2b_IBM_text_analysis/discovery_service.py
@@ -11,27 +11,15 @@ from watson_developer_cloud import DiscoveryV1
 #%%
 discovery = watson_developer_cloud.DiscoveryV1(
     '2016-11-07',
-    username='9f19e275-2600-4106-ab9e-c83fddbbb7ae',
-    password='5fB4Qaayeirf',
-    url= "https://gateway.watsonplatform.net/discovery/api")
+    username='68aa6cc2-6ac7-493f-8989-9f71a3b1a3c8',
+    password='NFjvZItut5Vu',
+    url= "https://gateway-fra.watsonplatform.net/discovery/api")
 
-#%%
+
 environments = discovery.list_environments()
 print(json.dumps(environments, indent=2))
-#%%
-'''news_environments = [x for x in environments['environments'] if
-                     x['name'] == 'Watson News Environment']
-news_environment_id = news_environments[0]['environment_id']
-print(json.dumps(news_environment_id, indent=2))
 
-#%%
-collections = discovery.list_collections(news_environment_id)
-news_collections = [x for x in collections['collections']]
-print(json.dumps(collections, indent=2))'''
-
-
-
-news_environments = [x for x in environments['environments'] if x['name'] == 'Watson Discovery Newws Environment']
+news_environments = [x for x in environments['environments'] if x['name'] == 'Anacredit']
 news_environment_id = news_environments[0]['environment_id']
 print(json.dumps(news_environment_id, indent=2))
 
@@ -39,9 +27,80 @@ collections = discovery.list_collections(news_environment_id)
 news_collections = [x for x in collections['collections']]
 print(json.dumps(news_collections, indent=2))
 
+configurations = discovery.list_configurations(
+    environment_id=news_environment_id)
+print(json.dumps(configurations, indent=2))
+
+
+query_options = {'query': 'IBM'}
+query_results = discovery.query(news_environment_id,
+                                news_collections[0]['collection_id'],
+                                query_options)
+
+news_collection_id = news_collections[0]['collection_id']            # AKTUELLER TEST !!!!!
+print(json.dumps(query_results, indent=2))
+
+if (discovery.get_environment(environment_id=news_environment_id)['status'] == 'active'):
+    writable_environment_id = news_environment_id
+
+    new_collection = discovery.create_collection(environment_id=writable_environment_id,
+                                                 name='Reports',
+                                                 description="Reports English Data")
+
+    print(new_collection)
+
+    with open(os.path.join(os.getcwd(), 'Reports', 'AkamaiFileDownload.pdf')) as fileinfo:
+        add_doc = discovery.add_document(environment_id=writable_environment_id, '{collection_id}', file=fileinfo)
+        print(discovery.test_document(environment_id=writable_environment_id, fileinfo=fileinfo))
+
+#    with open(os.path.join(os.getcwd(), '{path_element}', '{filename}')) as fileinfo:
+#        add_doc = discovery.add_document('{environment_id}', '{collection_id}', file=fileinfo)
+#    print(json.dumps(add_doc, indent=2))
 
 
 
+
+#if (discovery.get_environment(environment_id=environment['environment_id'])['status'] == 'active'):
+#    writable_environment_id = environment['environment_id']
+#
+#   new_collection = discovery.create_collection(environment_id=writable_environment_id,
+#                                                 name='Anacredit Collection',
+#                                                 description="Anacredit English Data")
+#
+#    print(new_collection)
+
+'''
+
+
+
+#%%
+environments = discovery.list_environments()
+print(json.dumps(environments, indent=2))
+#%%
+news_environments = [x for x in environments['environments'] if
+                     x['name'] == 'WatsonNewsEnvironment']
+news_environment_id = news_environments[0]['environment_id']
+print(json.dumps(news_environment_id, indent=2))
+
+#%%
+collections = discovery.list_collections(news_environment_id)
+news_collections = [x for x in collections['collections']]
+print(json.dumps(collections, indent=2))
+
+
+////
+news_environments = [x for x in environments['environments'] if x['name'] == 'Watson Discovery News Environment']
+news_environment_id = news_environments[0]['environment_id']
+print(json.dumps(news_environment_id, indent=2))
+
+collections = discovery.list_collections(news_environment_id)
+news_collections = [x for x in collections['collections']]
+print(json.dumps(news_collections, indent=2))
+////
+
+
+new_collection = discovery.create_collection(environment_id='{environment_id}', configuration_id='{configuration_id}', name='{collection_name}', description='{collection_desc}', language='{collection_lang}')
+print(json.dumps(new_collection, indent=2))
 
 
 
@@ -109,3 +168,5 @@ with open(os.path.join(os.getcwd(),'Anacredit','anacredit_faq.pdf')) as fileinfo
 
 #res = discovery.delete_environment(environment_id=writable_environment_id)
 #print(res)
+
+'''
